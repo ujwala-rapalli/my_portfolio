81fecf49c1f666ca23eeb657c0be6a26a94e8576
ujwala-rapalli <22211a6697@bvrit.ac.in>
2025-05-05 16:35:48 +0530

    updated

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,7 +3,7 @@ export const experiences = [
     id: 1,
     title: 'Full Stack Developer Intern',
     company: "Sassymonks Private Limited",
-    duration: "(Dec 2024 - Present)",
+    duration: "(Dec 2024 - May 2025)",
     description: "Developed and maintained web applications using HTML, CSS, JavaScript, React, and PHP. Worked on API integration, database management with MySQL, and backend development using NodeJS. Implemented UI/UX improvements and enhanced website responsiveness. Collaborated with a team to deploy scalable and efficient web solutions."
   }
 ];
